d7eae95578c6ad651f2c69a90734ab913b53ffa6
DESKTOP-NJ4NBMM\murra <murrayjenkins9001@gmail.com>
2024-02-16 14:20:15 -0500

    player mouse input and speed boost work correctly

--- Assets/Assignment/Scripts/Ship.cs
+++ Assets/Assignment/Scripts/Ship.cs
@@ -3,6 +3,7 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Ship : MonoBehaviour
 {
@@ -42,7 +43,16 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            speedBoost();
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            speed = 3;
+        }
+
         Vector2 updatedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         mousepts.Add(updatedPos);
@@ -52,5 +62,11 @@ public class Ship : MonoBehaviour
             lastpt = updatedPos;
         }
 
+    }
+    
+    void speedBoost()
+    {
+        speed = 5;
+        Debug.Log("Wheee!");
     }
 }
