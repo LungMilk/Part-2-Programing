d84a3a97ea9340db8bd14d01cf84ce2b038b16a0
placej <placej@OA-S141-27>
2024-02-21 14:17:51 -0500

    Task 1b complete Player selected changes color to be highlighted

---
+++ Assets/Week 7/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer rbSprite;
+
+    public Color defState;
+    public Color highlighted;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+        rbSprite = GetComponent<SpriteRenderer>();
+
+        //rbSprite.color = defState;
+        Selected(false);
+    }
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+    public void Selected(bool selected)
+    {
+        if (selected)
+        {
+            rbSprite.color = highlighted;
+        }
+        else
+        {
+            rbSprite.color = defState;
+        }
+    }
+}
